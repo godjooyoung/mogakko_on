669ebcf6238e3f25f69030efa2dd3672c4d47cbc
godjooyoung <xolovesjoo36@naver.com>
2023-05-24 11:27:43 +0900

    Feat : 회원가입 추가

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,87 @@
-import React from 'react';
+import React from 'react'
+import { useState } from 'react'
 
-function SignUp(props) {
+function SignUp() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [agreeTerms, setAgreeTerms] = useState(false);
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        //여기에서 회원가입 로직 처리: 입력값 백엔드로 전송
+        setEmail('');
+        setName('');
+        setPassword('');
+        setConfirmPassword('');
+        setAgreeTerms(false);
+    };
+
+
+
+
     return (
-        <div>
-            회원가입입니다.
-        </div>
-    );
-}
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        placeholder='이메일 입력'
+                        required
+                    />
+                </div>
+                <div>
+                    <label>이름</label>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='이름 입력'
+                        required //필수입력
+                    />
+                </div>
+                <div>
+                    <label>비밀번호</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder='비밀번호'
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        placeholder='비밀번호 확인'
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        checked={agreeTerms}
+                        onChange={(e) => setAgreeTErms(e.target.checked)}
+                        required
+                    />
+                    <label>약관에 동의합니다.</label>
+                </div>
 
-export default SignUp;
+
+
+                <button type="submit">회원가입을 위한 링크 연결</button>
+            </form>
+        </>
+    );
+};
+
+
+export default SignUp
