58ea302235ccd994f44daae904043c69d7b38bcb
godjooyoung <xolovesjoo36@naver.com>
2023-06-02 03:09:23 +0900

    Feat : 조회 추가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Login } from '../axios/api/login';
-import { useMutation } from 'react-query';
-import { getCookie } from '../cookie/Cookie';
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+import { login } from '../axios/api/login'
+import { useMutation } from 'react-query'
+import { getCookie } from '../cookie/Cookie'
 const SignIn = () => {
 
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwrodError, setPasswordError] = useState('');
-    const [loginError, setLoginError] = useState('');
-    const [islValidationEmail, setIsValidationEmail] = useState(false);
-    const [islValidationPassword, setIsValidationPassword] = useState(false);
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [passwrodError, setPasswordError] = useState('')
+    const [loginError, setLoginError] = useState('')
+    const [islValidationEmail, setIsValidationEmail] = useState(false)
+    const [islValidationPassword, setIsValidationPassword] = useState(false)
 
-    const signInMutation = useMutation(Login, {
+    const signInMutation = useMutation(login, {
         onSuccess: () => {
             if(getCookie("token")?true:false) navigate('/')
         }
@@ -30,27 +29,27 @@ const SignIn = () => {
     
     
     const emailChangeHandler = (e) => {
-        setEmail(e.target.value);
-        setEmailError('');
+        setEmail(e.target.value)
+        setEmailError('')
     };
 
     const passwordChangeHandler = (e) => {
-        setPassword(e.target.value);
-        setPasswordError('');
+        setPassword(e.target.value)
+        setPasswordError('')
     };
 
     // email state, password state 변경시 유효성 체크
     useEffect(()=>{
         // email 유효성 체크
         if (!email) {
-            // setEmailError('이메일을 입력해주세요');
+            // setEmailError('이메일을 입력해주세요')
             setIsValidationEmail(false)
         } else {
             if(!/\S+@\S+\.\S+/.test(email)){
-                setEmailError('올바른 이메일 형식이 아닙니다.');
+                setEmailError('올바른 이메일 형식이 아닙니다.')
                 setIsValidationEmail(false)
             }else{
-                setEmailError('');
+                setEmailError('')
                 setIsValidationEmail(true)
             }
         }
@@ -59,14 +58,14 @@ const SignIn = () => {
     useEffect(()=>{
         // password 유효성 체크
         if (!password) {
-            // setPasswordError('비밀번호를 입력해주세요.');
+            // setPasswordError('비밀번호를 입력해주세요.')
             setIsValidationPassword(false)
         }else{
             if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/.test(password)){
-                setPasswordError('비밀번호는 대소문자, 숫자, 특수문자를 포함한 8~16자리여야 합니다.');
+                setPasswordError('비밀번호는 대소문자, 숫자, 특수문자를 포함한 8~16자리여야 합니다.')
                 setIsValidationPassword(false)
             }else{
-                setPasswordError('');
+                setPasswordError('')
                 setIsValidationPassword(true)
             }
         }
