3ac59d263dadfd7618168bd99afde100cb2f4f23
shinheeje <dbfk2637@naver.com>
2023-06-02 00:46:47 +0900

    Feat: 로그인 기능 완료

---
+++ src/axios/api/login.js
@@ -0,0 +1,18 @@
+import instance from "../apiConfig";
+import { setCookie } from "../../cookie/Cookie"
+
+const Login = async (loginUserInfo) => {
+  try {
+    const response = await instance.post('/members/login', loginUserInfo)
+    if (response.status === 200 && response.data.message === '로그인 성공') {
+      // 토큰 받기
+      const token = response.headers["access_key"];
+      setCookie("token", token);
+      return Promise.resolve(response);
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {Login}
