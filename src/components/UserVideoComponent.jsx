409cd81b9cb7100ff8002991f96b5581c9b2e272
godjooyoung <xolovesjoo36@naver.com>
2023-06-02 18:10:26 +0900

    OpenVidu 버전변경, 사용자명 쿠키세팅

--- src/components/UserVideoComponent.jsx
+++ src/components/UserVideoComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import OpenViduVideoComponent from './OpenViduVideo';
+import { getCookie } from '../cookie/Cookie';
 
 function UserVideoComponent({ streamManager }) {
 
     // stream 속성의 connection.data 값을 파싱, 그 안에서 clientData 속성의 값을 반환 
     // 이 함수를 호출하면 현재 사용자의 닉네임을 가져옴.
-    const getNicknameTag = () => {
+    const getNicknameTag = async() => {
         // Gets the nickName of the user
-        return JSON.parse(streamManager.stream.connection.data).clientData;
+        const nickName = await getCookie('nickName')
+        return nickName
     }
 
     return (
