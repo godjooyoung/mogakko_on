f825d30ec4a3a7cabd0cd3dfa73b6606f4a4fb3a
godjooyoung <xolovesjoo36@naver.com>
2023-06-04 21:13:17 +0900

    Feat :  기술 선택지 갯수 줄임, 알람 리덕스 처리

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { getCookie, removeCookie } from '../../cookie/Cookie';
 import { EventSourcePolyfill } from "event-source-polyfill";
+import { useDispatch, useSelector } from 'react-redux';
+import { __alarmSender } from '../../redux/modules/alarm'
 
 function Header() {
     const [isLogin, setIsLogin] = useState(false)
     const [isAlarmWindowOpen, setIsAlarmWindowOpen] = useState(false)
-    const [sseChecker, setSseChecker] = useState(0)
     const navigate = useNavigate();
-    const [alarmData, setAlarmData] = useState(null);
+    const eventSourceRef = useRef(null);
 
     useEffect(() => {
         const checkLoginStatus = async () => {
@@ -24,10 +25,17 @@ function Header() {
         checkLoginStatus()
     })
 
+    const dispatcher = useDispatch()
+    // 전역에 등록된 알람 내역 가져오기
+    const alarmInfo = useSelector((state) => {
+        return state.alarmInfo
+    })
+
     useEffect(() => {
          // 세션 스토리지에서 SSE 구독 상태를 확인
         const isSubscribed = sessionStorage.getItem('isSubscribed');
         console.log("[INFO] SSE isSubscribed", isSubscribed)
+        console.log("[INFO] SSE alarmInfo", alarmInfo)
         
         if(isLogin && !isSubscribed){
             // 로그인 상태일때 최초 한번만 구독 실행
@@ -38,7 +46,7 @@ function Header() {
                 const EventSource = EventSourcePolyfill
                 if (isLogin && accessKey && !isSubscribed ) {
                     console.log("[INFO] SSE 구독요청 ")
-                    const eventSource = new EventSource(
+                    eventSourceRef.current = new EventSource(
                         //헤더에 토큰
                         `${process.env.REACT_APP_SERVER_URL}/subscribe`,
                             {
@@ -49,34 +57,37 @@ function Header() {
                             }
                     )
 
-                    console.log("[INFO] SSE",eventSource.withCredentials);
-                    console.log("[INFO] SSE",eventSource.readyState);
-                    console.log("[INFO] SSE",eventSource.url);
+                    console.log("[INFO] SSE",eventSourceRef.current.withCredentials);
+                    console.log("[INFO] SSE",eventSourceRef.current.readyState);
+                    console.log("[INFO] SSE",eventSourceRef.current.url);
                 
-                    if(eventSource.readyState === 1){
+                    if(eventSourceRef.current.readyState === 1){
                         console.log("[INFO] SSE connection 상태")
                     }
 
-                    eventSource.addEventListener('open', (event) => {
+                    eventSourceRef.current.addEventListener('open', (event) => {
                         console.log("[INFO] SSE connection opened", event)
                         // 연결이 열렸을 때 실행할 코드 작성
                         sessionStorage.setItem('isSubscribed', true);
                     })
 
-                    eventSource.addEventListener('message', (event) => {
+                    eventSourceRef.current.addEventListener('message', (event) => {
                         console.log("[INFO] SSE message event", event)
                         const data = event.data
                         console.log("[INFO] SSE message data ", data)
+                        dispatcher(__alarmSender(data))
                         // 메세지 응답 처리
                         // TODO 화면에 붙여주기
                         //EventStream Created. [memberId=1]
                         //{"id":7,"content":"변희준3님이 친구요청을 보냈습니다.","url":"/friend/request/determine","isRead":false,"senderId":3,"receiverId":2,"createdAt":"2023-06-03 18:41:21"}
                         //{"id":11,"content":"변희준5님이 친구요청을 보냈습니다.","url":"/friend/request/determine","isRead":false,"senderId":7,"receiverId":1,"createdAt":"2023-06-04 08:02:17"}
-                        setAlarmData(data);
+                        //setAlarmData(data);
                     })
                     return () => {
-                        sessionStorage.setItem('isSubscribed', false);
-                        eventSource.close(); // 컴포넌트 언마운트 시 SSE 연결 종료
+                        if (eventSourceRef.current && !isLogin) {
+                            sessionStorage.setItem('isSubscribed', false);
+                            eventSourceRef.current.close(); // 로그아웃 시 SSE 연결 종료
+                        }
                     };
                 }
             };
@@ -87,10 +98,11 @@ function Header() {
 
     // 알림 내용 컴포넌트 생성 함수
     const renderAlertComponent = () => {
-        if (alarmData) {
+        if (alarmInfo) {
+            console.log("alarmInfo..", alarmInfo)
             return (
                 <>
-                <AlearTitle>{alarmData}</AlearTitle>
+                <AlearTitle>{alarmInfo?.[0]}</AlearTitle>
                 <AlearTitle>test!@@@!</AlearTitle>
                 </>
             );
@@ -113,6 +125,9 @@ function Header() {
             await removeCookie('token')
             await removeCookie('nickName')
             await sessionStorage.removeItem('isSubscribed')
+            if (eventSourceRef.current) {
+                eventSourceRef.current.close(); // SSE 연결 종료
+            }
         }
         remove()
         navigate('/')
