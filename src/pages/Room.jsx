267cddefbcc775d5530156fb0bd87e541eb876f7
shinheeje <dbfk2637@naver.com>
2023-06-07 18:04:32 +0900

    Feat: 룸 페이지 음소거 아이콘 추가

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -719,11 +719,11 @@ function Room() {
                       null
                     }
                     <PubilsherVideoWrap onClick={() => handleMainVideoStream(publisher)} movePositon={position}>
-                      <UserVideoComponent streamManager={publisher} />
+                      <UserVideoComponent streamManager={publisher} audioEnabled={audioEnabled}/>
                       {subscribers.map((e, i) => (
                         <div key={e.id} onClick={() => handleMainVideoStream(e)}>
                           <span>{e.id}</span>
-                          <UserVideoComponent streamManager={e} />
+                          <UserVideoComponent streamManager={e} audioEnabled={audioEnabled}/>
                         </div>
                       ))}
                     </PubilsherVideoWrap>
@@ -741,7 +741,7 @@ function Room() {
 
                 {mainStreamManager !== undefined ? (
                   <MainStreamWrap>
-                    <UserVideoComponent streamManager={mainStreamManager} />
+                    <UserVideoComponent streamManager={mainStreamManager}/>
                   </MainStreamWrap>
                 ) : null}
               </PubilshSession>
@@ -1237,6 +1237,10 @@ export const MainStreamWrap = styled.div`
 
   span {
     font-size: 22px;
+  }
+
+  img {
+    display: none;
   }
 `
 
