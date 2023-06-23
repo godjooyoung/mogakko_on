c1d657a4654664685828dc3e4adb5dc7c1479644
shinheeje <dbfk2637@naver.com>
2023-06-23 10:33:01 +0900

    Feat: 룸 페이지 신고기능 추가

---
+++ src/axios/api/repost.js
@@ -0,0 +1,14 @@
+import { jwtInstance } from "../apiConfig"
+
+const reportUser = async (report) => {
+  //{"requestSenderNickname": String,"determineRequest": boolean}
+  // console.log("장미의 선택은..?", target)
+  try {
+    const response = await jwtInstance.post('/members/declare', report)
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export { reportUser }
